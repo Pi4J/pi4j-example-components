5b3bea0a364cedf4ebd34a50855dfe56d2edd354
Frank Delporte <frank@webtechie.be>
2021-10-06 15:32:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+
+ Pi4J V2 :: Java I/O Library for Raspberry Pi :: Serial example application
+===========================================================================
+
+GitHub Actions: 
+![Maven build](https://github.com/pi4j/pi4j-example-serial/workflows/Maven/badge.svg)
+
+This project contains a minimal example application which uses the Pi4J (V2) library 
+to communicate with a device overal a serial link. Full description is available on [the Pi4J website](https://v2.pi4j.com/getting-started/minimal-example-application)
+
+## PROJECT OVERVIEW
+
+The goal of the example project is to show how to set up a Pi4J Maven project for the 
+Raspberry Pi.
+
+## WIRING
+
+
+### TEST IN TERMINAL
+
+```
+$ sudo apt-get install gpsd gpsd-clients
+$ sudo service gpsd start
+$ sudo systemctl status gpsd.socket
+● gpsd.socket - GPS (Global Positioning System) Daemon Sockets
+   Loaded: loaded (/lib/systemd/system/gpsd.socket; enabled; vendor preset: enabled)
+   Active: active (running) since Tue 2021-10-05 16:03:17 CEST; 1min 37s ago
+   Listen: /var/run/gpsd.sock (Stream)
+           [::1]:2947 (Stream)
+           127.0.0.1:2947 (Stream)
+    Tasks: 0 (limit: 4915)
+   CGroup: /system.slice/gpsd.socket
+
+Oct 05 16:03:17 raspberrypi systemd[1]: Listening on GPS (Global Positioning System) Daemon Sockets.
+$ gpsmon /dev/ttyS0
+```
+
+It can take a while before you receive signal from enough satellites!
+
+![Screenshot oif gpsmon](assets/screenshot-gpsmon.png)
+
+## RUNTIME DEPENDENCIES
+
+This project uses Pi4J V.2 which has the following runtime dependency requirements:
+- [**SLF4J (API)**](https://www.slf4j.org/)
+- [**SLF4J-SIMPLE**](https://www.slf4j.org/)
+- [**PIGPIO Library**](http://abyz.me.uk/rpi/pigpio) (for the Raspberry Pi) - This 
+dependency comes pre-installed on recent Raspbian images.  However, you can also 
+download and install it yourself using the instructions found 
+[here](http://abyz.me.uk/rpi/pigpio/download.html).
+
+## BUILD DEPENDENCIES & INSTRUCTIONS
+
+This project can be built with [Apache Maven](https://maven.apache.org/) 3.6 
+(or later) and Java 11 OpenJDK (or later). These prerequisites must be installed 
+prior to building this project.  The following command can be used to download 
+all project dependencies and compile the Java module.  You can build this 
+project directly on a Raspberry Pi with Java 11+.  
+
+```
+mvn clean package
+```
+
+### Compiled application to run on the Raspberry Pi
+
+Once the build is complete and was successful, you can find the compiled 
+artifacts in the `target` (Maven) or `build` (Gradle) folder.  Specifically 
+all dependency modules (JARs) and a simple `run.sh` bash script will be located in the 
+`target/distribution` (Maven) or `build/distribution` (Gradle) folder.  
+
+These are all the required files needed to distribute (copy) to your
+Raspberry Pi to run this project.  If you are using the native bindings running 
+locally on the Raspberry Pi, then you make have to run the program using `sudo` 
+to gain the necessary access permissions to the hardware I/O. 
+
+This is the list of files created by the build process of this example application:
+
+* pi4j-core
+* pi4j-example-serial
+* pi4j-library-pigpio
+* pi4j-plugin-pigpio
+* pi4j-plugin-raspberrypi
+* slf4j-api
+* slf4j-simple
+* run.sh --> this is the actual start file which will run pi4j-example-minimal
+
+Make the run script executable and start it like this:
+
+```
+chmod +x run.sh
+sudo ./run.sh
+```
+
+## LICENSE
+
+ Pi4J Version 2.0 and later is licensed under the Apache License,
+ Version 2.0 (the "License"); you may not use this file except in
+ compliance with the License.  You may obtain a copy of the License at:
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
---
+++ src/main/java/com/pi4j/example/SerialExample.java
@@ -0,0 +1,121 @@
+package com.pi4j.example;
+
+/*-
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: EXAMPLE  :: Sample Code
+ * FILENAME      :  MinimalExample.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  https://pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2021 Pi4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.util.Console;
+
+/**
+ * <p>This example fully describes the base usage of Pi4J by providing extensive comments in each step.</p>
+ *
+ * @author Frank Delporte (<a href="https://www.webtechie.be">https://www.webtechie.be</a>)
+ * @version $Id: $Id
+ */
+public class SerialExample {
+
+    private static final String SERIAL_ADDRESS = "/dev/ttyS0";
+
+    private static Context pi4j;
+    private static Serial serial;
+
+    /**
+     * This application reads the data from a GPS module through a serial interface.
+     *
+     * @param args an array of {@link java.lang.String} objects.
+     * @throws java.lang.Exception if any.
+     */
+    public static void main(String[] args) throws Exception {
+        // Create Pi4J console wrapper/helper
+        // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
+        var console = new Console();
+
+        // Print program title/header
+        console.title("<-- The Pi4J Project -->", "Serial Example project");
+
+        // ------------------------------------------------------------
+        // Initialize the Pi4J Runtime Context
+        // ------------------------------------------------------------
+        // Before you can use Pi4J you must initialize a new runtime
+        // context.
+        //
+        // The 'Pi4J' static class includes a few helper context
+        // creators for the most common use cases.  The 'newAutoContext()'
+        // method will automatically load all available Pi4J
+        // extensions found in the application's classpath which
+        // may include 'Platforms' and 'I/O Providers'
+        pi4j = Pi4J.newAutoContext();
+
+        // ------------------------------------------------------------
+        // Output Pi4J Context information
+        // ------------------------------------------------------------
+        // The created Pi4J Context initializes platforms, providers
+        // and the I/O registry. To help you to better understand this
+        // approach, we print out the info of these. This can be removed
+        // from your own application.
+        // OPTIONAL
+        PrintInfo.printLoadedPlatforms(console, pi4j);
+        PrintInfo.printDefaultPlatform(console, pi4j);
+        PrintInfo.printProviders(console, pi4j);
+
+        // Here we will create I/O interface for the serial communication.
+        serial = pi4j.create(Serial.newConfigBuilder(pi4j)
+                .use_9600_N81()
+                .id("my-serial")
+                .device(SERIAL_ADDRESS)
+                .provider("pigpio-serial")
+                .build());
+
+        // OPTIONAL: print the registry
+        PrintInfo.printRegistry(console, pi4j);
+
+        while (serial.isOpen()) {
+            int available = serial.available();
+            if (available > 0) {
+                console.println(serial.readString(available));
+            }
+        }
+
+        console.println("Serial is no longer open");
+
+        // ------------------------------------------------------------
+        // Terminate the Pi4J library
+        // ------------------------------------------------------------
+        // We we are all done and want to exit our application, we must
+        // call the 'shutdown()' function on the Pi4J static helper class.
+        // This will ensure that all I/O instances are properly shutdown,
+        // released by the system and shutdown in the appropriate
+        // manner. Terminate will also ensure that any background
+        // threads/processes are cleanly shutdown and any used memory
+        // is returned to the system.
+
+        // Shutdown Pi4J
+        pi4j.shutdown();
+    }
+}
