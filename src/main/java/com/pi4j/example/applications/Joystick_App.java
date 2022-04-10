9dd9e8d8e2f87c9bb94ac3f044f0737aaa3ad208
RetoStutz <reto.stutz@students.fhnw.ch>
2022-04-10 11:03:40 +0200

    setup joystick

---
+++ src/main/java/com/pi4j/example/applications/Joystick_App.java
@@ -0,0 +1,13 @@
+package com.pi4j.example.applications;
+
+import com.pi4j.context.Context;
+import com.pi4j.example.Application;
+import com.pi4j.example.components.Joystick;
+
+public class Joystick_App implements Application {
+    @Override
+    public void execute(Context pi4j) {
+
+
+    }
+}
