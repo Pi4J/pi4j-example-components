e9483476764686d3d6e9142a46a34ec6d1b6dbb6
Mike <mike.schoder@students.fhnw.ch>
2022-05-08 18:06:33 +0200

    Init RGB Class

---
+++ src/main/java/com/pi4j/example/applications/RGBLed_App.java
@@ -0,0 +1,12 @@
+package com.pi4j.example.applications;
+
+import com.pi4j.context.Context;
+import com.pi4j.example.Application;
+import com.pi4j.example.components.PIN;
+
+public class RGBLed_App implements Application {
+    @Override
+    public void execute(Context pi4j) {
+        // Initialize the RGB
+    }
+}
