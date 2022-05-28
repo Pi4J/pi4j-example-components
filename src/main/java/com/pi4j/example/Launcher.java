b74480e750bf6cfeb2980227e554b30b03f5d646
Mike <mike.schoder@students.fhnw.ch>
2022-05-28 15:42:20 +0200

    cleanup, testing now works

--- src/main/java/com/pi4j/example/Launcher.java
+++ src/main/java/com/pi4j/example/Launcher.java
@@ -25,7 +25,7 @@ public final class Launcher implements Runnable {
      * This list must contain all applications which should be executable through the launcher.
      * Each class instance must implement the Application interface and gets automatically added as a subcommand.
      */
-    public static final List<Application> APPLICATIONS = new ArrayList<>(Arrays.asList(
+    public static final List<Application> APPLICATIONS = new ArrayList<Application>(Arrays.asList(
         new SimpleButton_App(),
         new SimpleLED_App(),
         new LEDButton_App(),
