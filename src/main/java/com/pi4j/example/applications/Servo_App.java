a4ac80ac3d8365f15923fedce7aebe3bf0618c85
RetoStutz <reto.stutz@students.fhnw.ch>
2022-05-29 02:27:26 +0200

    new servo class and application. Old code did not work linear and scaling was not intuitive. New control from 0 to 100 percent and or from -90 to + 90 °. In addition, a custom range can be defined. After that the servo moves only in this range. Template taken from CrowPi

--- src/main/java/com/pi4j/example/applications/Servo_App.java
+++ src/main/java/com/pi4j/example/applications/Servo_App.java
@@ -2,37 +2,54 @@ package com.pi4j.example.applications;
 
 import com.pi4j.context.Context;
 import com.pi4j.example.Application;
-import com.pi4j.example.components.Servo;
+import com.pi4j.example.components.ServoMotor;
 import com.pi4j.example.components.helpers.PIN;
 
 public class Servo_App implements Application {
     @Override
     public void execute(Context pi4j) {
 
-        //testing component has only a valid range in 45 degrees
-        int maxDegrees = 45;
-        //initialising component
-        Servo servo = new Servo(pi4j, PIN.PWM18, 500, 1500, maxDegrees);
+        // Initialize servo motor component
+        final var servoMotor = new ServoMotor(pi4j, PIN.PWM18.getPin());
 
-        logInfo("setting it to the lowest position");
-        servo.setMin();
+        // Demonstrate the percentage mapping on the servo
+        System.out.println("In 2 seconds, the servo motor will move to the left-most position which is 0%");
+        delay(2000);
+        servoMotor.setPercent(10);
+
+        System.out.println("In another 2 seconds, the servo motor will show 100% by moving to the right-most position");
+        delay(2000);
+        servoMotor.setPercent(90);
+
+        System.out.println("Last but not least, in 2 more seconds the servo will be centered to display 50%");
+        delay(2000);
+        servoMotor.setPercent(50);
+
+        // Sweep once from left to right using the setAngle function
+        System.out.println("We will sweep once to the left in 2 seconds...");
+        delay(2000);
+        servoMotor.setAngle(-80);
+
+        System.out.println("... and now to the right in 2 more seconds!");
+        delay(2000);
+        servoMotor.setAngle(80);
+
+        // Use a custom range for displaying the data
+        System.out.println("Imagine a pointer on the servo positioned above a label between -20ºC and +40ºC");
+        System.out.println("By using the setRange() method, we can automatically map our temperature range to the servo range!");
+        System.out.println("As an example, in five seconds the servo will show -10º which should be on the far left of the servo.");
+        delay(2000);
+
+        servoMotor.setRange(-20, +40); // This will define our range as values between -20 and +40
+        servoMotor.moveOnRange(-10); // This will map -10 based on the previously defined range
+
+        delay(2000);
+
+        //back to middle position
+        System.out.println("To finish the servo will be centered to display 50%");
+        servoMotor.setPercent(50);
+
+        // And this demo is over, sleep for a second to give the servo some time to position itself
         delay(1000);
-
-        logInfo("setting it to the center position");
-        servo.setCenter();
-        delay(1000);
-
-        logInfo("setting it to the highest position");
-        servo.setMax();
-        delay(1000);
-
-        logInfo("counting up from zero");
-        for (int i = 0; i < maxDegrees; i++) {
-            servo.setPositionDegrees(i);
-            delay(500);
-        }
-
-        logInfo("closing the connection, shutting down the pwm");
-        servo.close();
     }
 }
